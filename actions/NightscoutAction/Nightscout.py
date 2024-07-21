ffba8dcf40430f36b85282177e9545dba0e1810b
Lirycs228 <erik@hammon-mbe.de>
2024-07-21 19:20:23 +0200

    Make nightscout URL proper

--- actions/NightscoutAction/Nightscout.py
+++ actions/NightscoutAction/Nightscout.py
@@ -75,7 +75,7 @@ class Nightscout(ActionBase):
 
     def load_config_values(self):
         settings = self.get_settings()
-        self.nightscout_url.set_text(settings.get("nightscout_url", "localhost"))
+        self.nightscout_url.set_text(settings.get("nightscout_url", "http://localhost"))
         self.nightscout_token.set_text(settings.get("nightscout_token", "token"))
 
     def on_url_value_changed(self, nightscout_url):
