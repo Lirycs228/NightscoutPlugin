Fix wrong normalization

actions/NightscoutCombinedAction/NightscoutCombined.py
@@ -90,30 +90,30 @@ class NightscoutGraph(ActionBase):
                 minutes_since_beginn = divmod((entry_time - time_from).total_seconds(), 60)[0]
                 data[int(minutes_since_beginn)] = entry["sgv"]
 
+        data = (data - np.min(data)) / (np.max(data) - np.min(data))
+
         return data
     
     def build_graph(self, values):
         canvas = Image.new("RGB", (500, 500), color="black")
         draw = ImageDraw.Draw(canvas)
 
-        top_pad = 100
-        height_range = 300 # 50 bottom, 150 top
+        top_pad = 300
+        height_range = 200 # 50 bottom, 150 top
         left_pad = 50
         point_spacing = 2# assumption: 200 minutes in 400 pixels
         radius = 8
 
-        data = np.clip(values, 0, 300)
+        values = values*height_range
 
-        draw.line((0, top_pad, 500, top_pad), fill=(150, 150, 150), width=3)
-        draw.line((0, top_pad+height_range, 500, top_pad+height_range), fill=(150, 150, 150), width=3)
-        draw.line((0, top_pad+height_range-100, 500, top_pad+height_range-100), fill=(150, 0, 0), width=3)
+        draw.line((0, top_pad, 500, top_pad), fill=(100, 100, 100), width=3)
         
-        for count, (value, data) in enumerate(zip(values, data)):
+        for count, value in enumerate(values):
             if value != None and value != 0:
                 position = (   left_pad+(point_spacing*count)-radius, 
-                        top_pad+(height_range-int(data))-radius, 
+                        top_pad+(height_range-int(value))-radius, 
                         left_pad+(point_spacing*count)+radius, 
-                        top_pad+(height_range-int(data))+radius  )
+                        top_pad+(height_range-int(value))+radius  )
                 draw.ellipse(
                     position, 
                     fill=self.get_color(value))
@@ -132,14 +132,25 @@ class NightscoutGraph(ActionBase):
             )
             if entries != None:
                 if len(entries) > 0:
-                    current_time = datetime.now(timezone.utc)
-                    current_time = current_time.replace(microsecond=0)
-                    time_from = time_from = datetime.now(timezone.utc) - timedelta(minutes=200)
-                    graph = self.build_graph(self.extract_values(entries, time_from, current_time))
-                    self.set_media(image=graph)
+                    if entries[0]["type"] == "sgv":
+                        self.set_center_label(str(entries[0]["sgv"]) + " " + self.direction_to_arrow(entries[0]["direction"]), font_size=20)
+                        entry_time = parser.parse(entries[0]["dateString"])
+
+                        current_time = datetime.now(timezone.utc)
+                        current_time = current_time.replace(microsecond=0)
+                        time_delta_minutes = divmod((current_time - entry_time).total_seconds(), 60)[0]
+                        self.set_top_label(str(int(time_delta_minutes)) + " m", font_size=16)
+
+                        time_from = time_from = datetime.now(timezone.utc) - timedelta(minutes=200)
+                        graph = self.build_graph(self.extract_values(entries, time_from, current_time))
+                        self.set_media(image=graph)
                 else:
+                    self.set_center_label("no data", font_size=18)
+                    self.set_top_label("")
                     self.set_media(image=Image.new("RGB", (500, 500), color="black"))
             else:
+                self.set_center_label("no data", font_size=18)
+                self.set_top_label("")
                 self.set_media(image=Image.new("RGB", (500, 500), color="black"))
     
     def get_config_rows(self) -> list: